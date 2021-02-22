3bad04f2b62614d85047053a9ac00dd5f63d1462
ikatz87 <ilija.pavlovic87@gmail.com>
2021-02-23 00:49:56 +0100

    Put app.set///app.use where thay belong under the app variable

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const bodyParser = require("body-parser");
 const ejs = require("ejs");
 
 const app = express();
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
 
 app.get("/", function (req, res) {
   res.send(Hello);
@@ -11,8 +14,3 @@ app.get("/", function (req, res) {
 app.listen(3000, function () {
   console.log("Server started on port 3000");
 });
-
-app.set("view engine", "ejs");
-
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static("public"));
