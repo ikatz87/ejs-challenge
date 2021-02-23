162e530bd1cd5cb2477e38b00e166aa14b4f3d66
ikatz87 <ilija.pavlovic87@gmail.com>
2021-02-23 23:25:04 +0100

    Finish Challenge 18

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const _ = require("lodash");
 
 const homeStartingContent =
   "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
@@ -45,7 +46,7 @@ app.post("/compose", (req, res) => {
 app.get(`/posts/:postName`, (req, res) => {
   let urlToCheck = req.params.postName;
   AllPosts.forEach((posts) => {
-    posts.title === urlToCheck
+    _.lowerCase(posts.title) === _.lowerCase(urlToCheck)
       ? console.log("Match found!!")
       : console.log("Not Found!!");
   });
