f99b7a15a2a5e4de979f38a13f01d6c1d3c47890
ikatz87 <ilija.pavlovic87@gmail.com>
2021-02-23 15:17:19 +0100

    Add routes to contact/about pages in app.js width different content

--- app.js
+++ app.js
@@ -4,14 +4,23 @@ const ejs = require("ejs");
 
 const homeStartingContent =
   "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
-
+const aboutContent =
+  "About Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
+const contactContent =
+  "Contact Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
 const app = express();
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-  res.render("home.ejs", { pDefault: homeStartingContent });
+  res.render("home.ejs", { pDefault: homeStartingContent, hDefault: "Home" });
+});
+app.get("/about", (req, res) => {
+  res.render("about.ejs", { pDefault: aboutContent, hDefault: "About" });
+});
+app.get("/contact", (req, res) => {
+  res.render("contact.ejs", { pDefault: contactContent, hDefault: "Contact" });
 });
 
 app.listen(3000, () => {
